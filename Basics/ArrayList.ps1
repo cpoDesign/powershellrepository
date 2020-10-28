d76618d1b6f3527d2db2f6030bdfc37aa116254a
Pavel <cpoDesign@users.noreply.github.com>
2020-10-28 00:12:44 +0000

    Update ArrayList.ps1
    Added additional option with object definition and different way of displaying data

--- Basics/ArrayList.ps1
+++ Basics/ArrayList.ps1
@@ -7,3 +7,19 @@ $arr+= @{ name = 'Joe'; Value = 32; }
 for ($i=0; $i -lt $arr.length; $i++) {
 	$arr[$i]['name']
 }
+
+
+#alternative way
+
+$allRecords = @()
+$allRecords+=([pscustomobject]@{Name='function1';Url='https://function1'})
+$allRecords+=([pscustomobject]@{Name='function2';Url='https://function2'})
+
+#access properties using foreach
+foreach( $row in $allRecords ){
+
+	Write-Host $row.Name
+}
+
+# list items
+$allRecords | Format-List
